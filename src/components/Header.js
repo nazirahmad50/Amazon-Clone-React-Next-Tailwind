5eae93f105ed9bcebd7c3375b5ebe1892db56591
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-19 16:09:22 +0100

    - Create Header template - Create next config for Image tag

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* Bottom Nav */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
