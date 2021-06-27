1a1b980dfc1b5d305af9e1017cf542e557010868
Nazir Ahmad <n_ahmad20@live.co.uk>
2021-06-27 15:24:36 +0100

    Update envs Install firebase Install and use next auth

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUJBt7RFBbK-lbcBiULJFiao05Yq4umK8",
+  authDomain: "amazn-clone-c3ae7.firebaseapp.com",
+  projectId: "amazn-clone-c3ae7",
+  storageBucket: "amazn-clone-c3ae7.appspot.com",
+  messagingSenderId: "453355536244",
+  appId: "1:453355536244:web:b6849ff1138701630d7621",
+};
